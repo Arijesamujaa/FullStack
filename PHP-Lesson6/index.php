cc2a233a82d649ba9dfdf05f4cdcbc26cb9c69de
Arijesamujaa <156703550+Arijesamujaa@users.noreply.github.com>
2024-12-02 12:18:27 +0100

    Update index.php

--- PHP-Lesson6/index.php
+++ PHP-Lesson6/index.php
@@ -1,16 +1,42 @@
 <?php
 
 $host = 'localhost';
+$db = 'test3';
 $user = 'root';
 $pass = '';
 
 try{
-    $conn = new PDO("mysql:host=$host;", $user, $pass);
+    $conn = new PDO("mysql:host=$host; dbname=$db", $user, $pass);
 
-    $sql = "CREATE DATABASE test3";
+    //set values to be inseted
+    //$username = "Arijesa";
+    //$password = password_hash("mypassword", PASSWORD_DEFAULT);
+
+    //$sql = "INSERT INTO users(username, password) values ('$username', '$password')";
+
+    //$sql = "CREATE TABLE users(id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    //username VARCHAR(255) NOT NULL,
+    //password VARCHAR(255) NOT NULL )";
+
+    //adding columns
+    //$sql = "ALTER TABLE users ADD email varchar(255)";
+
+    //deleting columns
+    //$sql = "ALTER TABLE users DROP column email";
+
+
+    //delete table
+    $sql = "DROP TABLE users";
+
+
+
     $conn->exec($sql);
 
-    echo "Database is created";
+
+    //$last_id = $conn -> lastInsertId();
+    //echo "User is created". $last_id;
+
+    echo "Column is created";
 } catch (Exception $e){
     echo "Something went wrong";
 }
